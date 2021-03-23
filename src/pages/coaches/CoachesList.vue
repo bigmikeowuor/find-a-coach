62e966f76079fea206ece2ef8171f6a65adabb65
Michael Owuor <michael.owuor@gmail.com>
2021-03-23 13:08:29 +0300

    Adding base components.

--- src/pages/coaches/CoachesList.vue
+++ src/pages/coaches/CoachesList.vue
@@ -1,22 +1,24 @@
 <template>
 	<section>Filter</section>
 	<section>
-		<div class="controls">
-			<button>Refresh</button>
-			<router-link to="/register">Register as a Coach</router-link>
-		</div>
-		<ul v-if="hasCoaches">
-			<coach-item
-				v-for="coach in filteredCoaches"
-				:key="coach.id"
-				:id="coach.id"
-				:first-name="coach.firstName"
-				:last-name="coach.lastName"
-				:rate="coach.hourlyRate"
-				:areas="coach.areas"
-			></coach-item>
-		</ul>
-		<h3 v-else>No coaches found.</h3>
+		<base-card>
+			<div class="controls">
+				<base-button mode="outline">Refresh</base-button>
+				<base-button link to="/register">Register as a Coach</base-button>
+			</div>
+			<ul v-if="hasCoaches">
+				<coach-item
+					v-for="coach in filteredCoaches"
+					:key="coach.id"
+					:id="coach.id"
+					:first-name="coach.firstName"
+					:last-name="coach.lastName"
+					:rate="coach.hourlyRate"
+					:areas="coach.areas"
+				></coach-item>
+			</ul>
+			<h3 v-else>No coaches found.</h3>
+		</base-card>
 	</section>
 </template>
 
@@ -24,7 +26,9 @@
 import CoachItem from '../../components/coaches/CoachItem.vue';
 
 export default {
-	components: { CoachItem },
+	components: {
+		CoachItem,
+	},
 
 	computed: {
 		hasCoaches() {
