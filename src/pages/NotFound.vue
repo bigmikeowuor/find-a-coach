d84b4ea93698703c7b04f95b2e5f9d1e5ac7eab2
Michael Owuor <michael.owuor@gmail.com>
2021-03-26 12:38:10 +0300

    The not-found page.

---
+++ src/pages/NotFound.vue
@@ -0,0 +1,17 @@
+<template>
+	<section>
+		<base-card>
+			<h2>Page not found</h2>
+			<p>
+				The page you are looking for could not be found. Please try the
+				<router-link to="/coaches">coaches</router-link> link.
+			</p>
+		</base-card>
+	</section>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
