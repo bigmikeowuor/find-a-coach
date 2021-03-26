343bbbff18fe24d9e16ff30199ce6737baaea13d
Michael Owuor <michael.owuor@gmail.com>
2021-03-26 11:02:08 +0300

    Caching HTTP response data.

--- src/store/modules/coaches/actions.js
+++ src/store/modules/coaches/actions.js
@@ -29,7 +29,11 @@ export default {
 		});
 	},
 
-	async loadCoaches(context) {
+	async loadCoaches(context, payload) {
+		if (!payload.forceRefresh && !context.getters.shouldUpdate) {
+			return;
+		}
+
 		const response = await fetch(
 			`https://find-a-coach-mo-default-rtdb.europe-west1.firebasedatabase.app/coaches.json`
 		);
@@ -57,5 +61,6 @@ export default {
 		}
 
 		context.commit('setCoaches', coaches);
+		context.commit('setFetchTimestamp');
 	},
 };
