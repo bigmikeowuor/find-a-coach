02e138e36b30f41c4fe2322ac681391b555bf321
Michael Owuor <michael.owuor@gmail.com>
2021-03-31 01:41:50 +0300

    Preparing Vuex.

--- src/store/index.js
+++ src/store/index.js
@@ -2,23 +2,13 @@ import { createStore } from 'vuex';
 
 import coachesModule from './modules/coaches/index.js';
 import requestsModule from './modules/requests/index.js';
+import authModule from './modules/auth/index.js';
 
 const store = createStore({
-	state() {
-		return {
-			userId: 'c3',
-		};
-	},
-
 	modules: {
 		coaches: coachesModule,
 		requests: requestsModule,
-	},
-
-	getters: {
-		userId(state) {
-			return state.userId;
-		},
+		auth: authModule,
 	},
 });
 
